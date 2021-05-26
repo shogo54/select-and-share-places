f30f30ee3e61cf46e8c0767d237d5e0fa0e383ce
Shogo Akiyama <sakiyama@knox.edu>
2021-05-26 00:23:48 -0700

    Add Google Geocoding type and basic fetch error handling

--- src/app.ts
+++ src/app.ts
@@ -6,21 +6,31 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = config.googleApiKey as string;
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   // send this to Google's API
   axios
-    .get(
+    .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     )
     .then((response) => {
-      console.log(response);
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
     })
     .catch((err) => {
+      alert(err.message);
       console.log(err);
     });
 }
