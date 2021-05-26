e0ffb161cd3c33965769e4c4332635588248867a
Shogo Akiyama <sakiyama@knox.edu>
2021-05-26 00:08:45 -0700

    Add submit function to fetch address data from Google Map API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
+import axios from 'axios';
+import config from '../config.json';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = config.googleApiKey as string;
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+
+  // send this to Google's API
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-form?.addEventListener('submit',searchAddressHandler);
+form?.addEventListener('submit', searchAddressHandler);
